32012891bbc5802b80b0e9cb132ff5e35fc5107c
Petya Todorova <invisioncl@gmail.com>
2018-03-12 14:02:08 +0200

    Added TeamWork.sln with 2 projects: Pr1DebitCardNumber.csproj and Pr2RectangleArea.csproj

---
+++ intro-and-basic-syntax/exercise/TeamWork/Pr1DebitCardNumber/DebitCardNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pr1DebitCardNumber
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            var fNum = int.Parse(Console.ReadLine());
+            var sNum = int.Parse(Console.ReadLine());
+            var tNum = int.Parse(Console.ReadLine());
+            var fourNum = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{fNum:D4} {sNum:D4} {tNum:D4} {fourNum:D4}");
+        }
+    }
+}
