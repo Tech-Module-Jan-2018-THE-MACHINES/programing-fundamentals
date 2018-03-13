7aa40564b22e339d24c57bedce4b61731a10e8bb
Roman Gyolski <roman__@abv.bg>
2018-03-13 11:58:19 +0200

    add folder exercise2

---
+++ intro-and-basic-syntax/exercise2/SolutionForProblem1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var fNum = int.Parse(Console.ReadLine());
+        var sNum = int.Parse(Console.ReadLine());
+        var tNum = int.Parse(Console.ReadLine());
+        var fourNum = int.Parse(Console.ReadLine());
+        Console.WriteLine($"{fNum:D4} {sNum:D4} {tNum:D4} {fourNum:D4}");
+    }
+}
