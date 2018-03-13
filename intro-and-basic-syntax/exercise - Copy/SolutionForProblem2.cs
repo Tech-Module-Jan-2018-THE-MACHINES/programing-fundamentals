a2e248ddf9fdd0381c2cfc8750dcbcdaf155bad9
Petya Todorova <invisioncl@gmail.com>
2018-03-13 11:55:55 +0200

    Created exercise - Copy/SolutionForProblem1.cs Created exercise - Copy/SolutionForProblem2.cs

---
+++ intro-and-basic-syntax/exercise - Copy/SolutionForProblem2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pr2RectangleArea
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double widthRectangle = double.Parse(Console.ReadLine());
+            double heightRectangle = double.Parse(Console.ReadLine());
+            double areaRectangle = widthRectangle * heightRectangle;
+            Console.WriteLine($"{areaRectangle:F2}");
+        }
+    }
+}
