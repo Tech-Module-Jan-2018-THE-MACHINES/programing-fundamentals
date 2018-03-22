3aee495302ba6a23563013d44a9d9f3edc6406c6
Roman Gyolski <roman__@abv.bg>
2018-03-22 22:21:38 +0200

    Add problem 4 and 5

---
+++ intro-and-basic-syntax/exercise/SolutionForProblem5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        var name = Console.ReadLine();
+        var health = int.Parse(Console.ReadLine());
+        var maxHealth = int.Parse(Console.ReadLine());
+        var energy = int.Parse(Console.ReadLine());
+        var maxEnergy = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine("Health: |{0}{1}|", new string('|', health), new string('.', maxHealth - health));
+        Console.WriteLine("Energy: |{0}{1}|", new string('|', energy), new string('.', maxEnergy - energy));
+    }
+}
