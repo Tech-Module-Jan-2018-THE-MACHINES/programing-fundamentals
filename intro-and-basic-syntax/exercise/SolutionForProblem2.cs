e577f96e6f6d121aebc185c2cf5f0c97b12531a0
Roman Gyolski <roman__@abv.bg>
2018-03-22 22:41:19 +0200

    documents the code of Solution of Problem 2

--- intro-and-basic-syntax/exercise/SolutionForProblem2.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Pr2RectangleArea
 {
@@ -6,9 +6,13 @@ namespace Pr2RectangleArea
     {
         static void Main(string[] args)
         {
+            //In the first line we reciev width;
             double widthRectangle = double.Parse(Console.ReadLine());
+            //In the second line we reciev height;
             double heightRectangle = double.Parse(Console.ReadLine());
+            //We calculate area
             double areaRectangle = widthRectangle * heightRectangle;
+            //Print result
             Console.WriteLine($"{areaRectangle:F2}");
         }
     }
