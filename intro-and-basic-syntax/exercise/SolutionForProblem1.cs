2e8b266fce8997ed778e4b680c4ebd3f4c0ab73e
Roman Gyolski <roman__@abv.bg>
2018-03-22 22:48:37 +0200

    documents the code of Solution of Problem 1

--- intro-and-basic-syntax/exercise/SolutionForProblem1.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem1.cs
@@ -1,13 +1,18 @@
-﻿using System;
+using System;
 
 public class Program
 {
     public static void Main()
     {
+        // Recieve num
         var fNum = int.Parse(Console.ReadLine());
+        // Recieve num
         var sNum = int.Parse(Console.ReadLine());
+        // Recieve num
         var tNum = int.Parse(Console.ReadLine());
+        // Recieve num
         var fourNum = int.Parse(Console.ReadLine());
+        // Print num
         Console.WriteLine($"{fNum:D4} {sNum:D4} {tNum:D4} {fourNum:D4}");
     }
 }
