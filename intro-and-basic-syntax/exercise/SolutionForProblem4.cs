bbc85d5377498e6765c1640e3ffbfd3fc128b62d
todor <todorstanchev@gmail.com>
2018-03-23 01:38:48 +0200

    documents the code of Solution of Problem

--- intro-and-basic-syntax/exercise/SolutionForProblem4.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem4.cs
@@ -3,12 +3,17 @@
 public class Program
 {
     public static void Main()
-    {
+    {   //reading from the console product name 
         var name = Console.ReadLine();
+        //reading from the console product volume
         var volume = double.Parse(Console.ReadLine());
+        //reading from the console product energy content per 100ml 
         var energy = double.Parse(Console.ReadLine());
+        //reading from the console product sugar content per 100ml 
         var sugar = double.Parse(Console.ReadLine());
+        //printing volume and name
         Console.WriteLine($"{volume}ml {name}:");
+        //calculating and print kcal and sugar
         Console.WriteLine($"{(energy * volume / 100)}kcal, {(sugar * volume / 100)}g sugars");
     }
 }
