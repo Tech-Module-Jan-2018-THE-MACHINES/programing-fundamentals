ae8fe2e607d7ab59341d97c3b3ebe1a424409a75
Petya Todorova <invisioncl@gmail.com>
2018-03-23 09:32:02 +0200

    I've changed the names of the variables in SolutionForProblem4.cs.

--- intro-and-basic-syntax/exercise/SolutionForProblem4.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem4.cs
@@ -4,16 +4,16 @@ public class Program
 {
     public static void Main()
     {   //reading from the console product name 
-        var name = Console.ReadLine();
+        var productName = Console.ReadLine();
         //reading from the console product volume
         var volume = double.Parse(Console.ReadLine());
         //reading from the console product energy content per 100ml 
-        var energy = double.Parse(Console.ReadLine());
+        var energyContent = double.Parse(Console.ReadLine());
         //reading from the console product sugar content per 100ml 
-        var sugar = double.Parse(Console.ReadLine());
+        var sugarContent = double.Parse(Console.ReadLine());
         //printing volume and name
-        Console.WriteLine($"{volume}ml {name}:");
+        Console.WriteLine($"{volume}ml {productName}:");
         //calculating and print kcal and sugar
-        Console.WriteLine($"{(energy * volume / 100)}kcal, {(sugar * volume / 100)}g sugars");
+        Console.WriteLine($"{(energyContent * volume / 100)}kcal, {(sugarContent * volume / 100)}g sugars");
     }
 }
