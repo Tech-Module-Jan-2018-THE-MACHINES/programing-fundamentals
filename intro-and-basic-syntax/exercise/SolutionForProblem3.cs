1fba38a6328af20eed410e7e226f9c0422a31bd7
Petya Todorova <invisioncl@gmail.com>
2018-03-23 09:52:22 +0200

    Change of the variable kilometersPerMile to kmPerMiles in SolutionForProblem3.cs.

--- intro-and-basic-syntax/exercise/SolutionForProblem3.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem3.cs
@@ -9,9 +9,9 @@ namespace miles
             // We are reading the distance in miles from the Console
             var distanceInMiles = double.Parse(Console.ReadLine());
             // We make a const for the prorortion km to miles
-            const double kilometersPerMile = 1.60934;
+            const double kmPerMiles = 1.60934;
             // The next line calculates the distance in km
-            var distanceInKilometers = distanceInMiles * kilometersPerMile;
+            var distanceInKilometers = distanceInMiles * kmPerMiles;
             //Printing the result
             Console.WriteLine("{0:F2}", distanceInKilometers);
         }
