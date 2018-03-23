18534ffadb20574a464123c44fa47b472bdd1444
Petya Todorova <invisioncl@gmail.com>
2018-03-23 09:10:45 +0200

    Added comments to SolutionForProblem3.cs.

--- intro-and-basic-syntax/exercise/SolutionForProblem3.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem3.cs
@@ -6,10 +6,13 @@ namespace miles
     {
         public static void Main(string[] args)
         {
+            // We are reading the distance in miles from the Console
             var distanceInMiles = double.Parse(Console.ReadLine());
+            // We make a const for the prorortion km to miles
             const double kilometersPerMile = 1.60934;
+            // The next line calculates the distance in km
             var distanceInKilometers = distanceInMiles * kilometersPerMile;
-
+            //Printing the result
             Console.WriteLine("{0:F2}", distanceInKilometers);
         }
     }
