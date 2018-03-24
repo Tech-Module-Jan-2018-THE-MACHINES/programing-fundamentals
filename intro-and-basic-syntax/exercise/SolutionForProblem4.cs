248cca7a9e8b2b45aa7fa366e561748e424a8d70
Petya Todorova <invisioncl@gmail.com>
2018-03-24 11:19:07 +0200

    Small design correction to the comments of SolutionForProblem4.cs.

--- intro-and-basic-syntax/exercise/SolutionForProblem4.cs
+++ intro-and-basic-syntax/exercise/SolutionForProblem4.cs
@@ -5,17 +5,22 @@ using System;
 public class Program
 {
     public static void Main()
-    {   //reading from the console product name 
+    {   //Reading from the console product name 
         var productName = Console.ReadLine();
-        //reading from the console product volume
+
+        //Reading from the console product volume
         var volumeForProduct = double.Parse(Console.ReadLine());
-        //reading from the console product energy content per 100ml 
+
+        //Reading from the console product energy content per 100ml 
         var energyContentPer100ml = double.Parse(Console.ReadLine());
-        //reading from the console product sugar content per 100ml 
+
+        //Reading from the console product sugar content per 100ml 
         var sugarContentPer100ml = double.Parse(Console.ReadLine());
-        //printing volume and name
+
+        //Printing volume and name
         Console.WriteLine($"{volumeForProduct}ml {productName}:");
-        //calculating and print kcal and sugar
+
+        //Calculating and print kcal and sugar
         Console.WriteLine($"{(energyContentPer100ml * volumeForProduct / 100)}kcal, {(sugarContentPer100ml * volumeForProduct / 100)}g sugars");
     }
 }
